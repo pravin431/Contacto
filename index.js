0bbaf97ba5105a5697b47906df677bbc90a50e12
Pravin Jedhe <pravinjedhe12@gmail.com>
2024-06-25 01:28:43 +0530

    update index.js

--- index.js
+++ index.js
@@ -12,11 +12,6 @@ mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
-// Root route to display a message
-app.get('/', (req, res) => {
-    res.send('<h1>Express Testing</h1><p>App is running properly</p>');
-});
-
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 3000;
